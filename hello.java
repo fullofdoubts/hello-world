6848456e2d5589c6ca6ea11b4fcdff282263821d
jingwei <jingwei@WeiJing>
2019-02-28 18:10:24 +0800

    stes

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hellworld{
   public static void main( String args[]){
     System.out.println("hello world");
     System.out.println("Hello2");
+    System.out.println("Hello3");
     }
   }
 }
